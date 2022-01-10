af1a77d0081d85b22ee6209d8edb283e0cee8f9d
marshelino-maged <78962756+marshelino-maged@users.noreply.github.com>
2022-01-10 20:14:08 +0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -4,3 +4,49 @@ typedef struct{
     char name[100];
 }player;
 
+
+typedef struct{
+    int score;
+    char name[10];
+    }gamer;
+
+typedef struct{
+    int i;
+    int oneScore;
+    int oneMoves;
+    int twoScore;
+    int twoMoves;
+    char array1[5][5];
+}undo1;
+
+typedef struct{
+    int i;
+    int oneScore;
+    int oneMoves;
+    int twoScore;
+    int twoMoves;
+    char array1[5][5];
+}redo1;
+
+typedef struct{
+    int i;
+    int oneScore;
+    int oneMoves;
+    int twoScore;
+    int twoMoves;
+    char array1[9][9];
+}undo2;
+
+typedef struct{
+    int i;
+    int oneScore;
+    int oneMoves;
+    int twoScore;
+    int twoMoves;
+    char array1[9][9];
+}redo2;
+
+
+
+
+
